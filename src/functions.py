b15014e47a76f1e6d3773c5c0daff362d3016986
ryo.ishii <ryoishii1101@gmail.com>
2020-01-06 00:42:01 +0900

    Update: bug fix

--- src/functions.py
+++ src/functions.py
@@ -12,9 +12,8 @@ def softmax(x):
         x = np.exp(x)
         x /= x.sum(axis=1, keepdims=True)
     elif x.ndim == 1:
-        x = np.max(x)
-        x = np.exp(x)
-        x /= np.sum(x)
+        x = x - np.max(x)
+        x = np.exp(x) / np.sum(np.exp(x))
     return x
 
 
