5ca4e9de72a834fc3374fbf1c3fd480bfd3e3206
ryo.ishii <ryoishii1101@gmail.com>
2020-01-05 13:42:28 +0900

    Update: Impl Better RNN LM

--- ch_06.py
+++ ch_06.py
@@ -1,11 +1,11 @@
 from datasets import ptb
 from src.optimizers import SGD
-from src.models import RNNLM
+from src.models import RNNLM, BetterRNNLM
 from src.trainer import RNNLMTrainer
 from src.utils import eval_perplexity
 
 
-def main():
+def train_rnnlm():
     batch_size = 20
     wordvec_size = 100
     hidden_size = 100
@@ -35,5 +35,46 @@ def main():
     model.save_params()
 
 
+def train_better_rnnlm():
+    batch_size = 20
+    wordvec_size = 650
+    hidden_size = 650
+    time_size = 35
+
+    lr = 20.0
+    max_epoch = 40
+    max_grad = 0.25
+    dropout = 0.5
+
+    corpus, word_to_id, id_to_word = ptb.load_data('train')
+    corpus_test, _, _ = ptb.load_data('test')
+    vocab_size = len(word_to_id)
+
+    xs = corpus[:-1]
+    ts = corpus[1:]
+
+    model = BetterRNNLM(vocab_size, wordvec_size, hidden_size, dropout)
+    optimizer = SGD(lr)
+
+    trainer = RNNLMTrainer(model, optimizer)
+
+    best_ppl = float('inf')
+    for epoch in range(max_epoch):
+        trainer.fit(xs, ts, 1, batch_size, time_size, max_grad)
+
+        model.reset_state()
+        ppl = eval_perplexity(model, corpus_test)
+        print(f'test perplexity: {ppl}')
+
+        if ppl > best_ppl:
+            best_ppl = ppl
+            model.save_params('better_RNN_params.pkl')
+        else:
+            lr /= 4.0
+            optimizer.lr = lr
+        model.reset_state()
+
+
 if __name__ == '__main__':
-    main()
+    # train_rnnlm()
+    train_better_rnnlm()
